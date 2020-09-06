bacac4e365a67089ff929e3420d200898710a73b
kellytbn <kellytbn14@gmail.com>
2020-09-06 15:00:03 -0500

    Se generan los componentes y services para producto y proveedor

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+import { ProveedorComponent } from './proveedor/proveedor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoComponent,
+    ProveedorComponent
   ],
   imports: [
     BrowserModule,
