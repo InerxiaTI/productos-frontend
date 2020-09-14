9f3aefb1c8d3f92f9e06508258d550c3dbc5b879
kellytbn <kellytbn14@gmail.com>
2020-09-13 22:27:30 -0500

    Se agrega el formulario de productos

--- code/src/app/form-producto/form-producto.component.ts
+++ code/src/app/form-producto/form-producto.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {Location} from '@angular/common';
-import {RadioButtonModule} from 'primeng/radiobutton';
+import { Location } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 import { ProductoService } from '../shared/service/producto.service';
+import { ProveedorService } from '../shared/service/proveedor.service';
+import { ProveedorDto } from '../shared/dto/ProveedorDto';
 
 @Component({
   selector: 'app-form-producto',
@@ -12,21 +15,59 @@ import { ProductoService } from '../shared/service/producto.service';
 export class FormProductoComponent implements OnInit {
 
   selectedIva: any = null;
+  proveedoresDto: Array<ProveedorDto> = [];
+  proveedorDto: ProveedorDto;
+  today = new Date();
 
   ivas: any[] = [
-    {name: 'IVA 19%', key: 'A'}, 
-    {name: 'IVA 10%', key: 'M'}, 
-    {name: 'IVA 5%', key: 'P'}, 
-    {name: 'NO', key: 'N'}];
+    { name: 'IVA 19%', key: 'A' },
+    { name: 'IVA 10%', key: 'M' },
+    { name: 'IVA 5%', key: 'P' },
+    { name: 'NO', key: 'N' }];
+
+  productoForm = new FormGroup({
+    nombre: new FormControl('', Validators.required),
+    descripcion: new FormControl(''),
+    precio_compra: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]{0,6})?$')]),
+    precio_venta: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]{0,6})?$')]),
+    iva: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+(.[0-9]{0,6})?$')]),
+    marca: new FormControl(''),
+    fecha_compra: new FormControl('', Validators.required),
+    foto: new FormControl(''),
+    idProveedor_fk: new FormControl('', Validators.required)
+  });
 
   constructor(
     private productoService: ProductoService,
+    private proveedorService: ProveedorService,
     private router: Router,
     private _location: Location
-  ) { }
+  ) {
+    this.today= new Date();
+   }
 
   ngOnInit(): void {
-    this.selectedIva = this.ivas[1];
+    this.getAllProveedores();
+    console.log(this.today);
+    
+    
+  }
+
+  getAllProveedores() {
+    this.proveedorService.getAllProveedores().subscribe(data => {
+      if (!data.body) {
+        return;
+      }
+      this.proveedoresDto = data.body;
+      console.log(this.proveedoresDto);
+    }, error => {
+      console.log('Error'.concat(error));
+    }, () => {
+    });
+  }
+  guardar(){
+    console.log(this.productoForm.value.fecha_compra);
+    
   }
 
   goBack() {
